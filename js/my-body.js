413b3155dc3937f9dc2dbb762235cc31957c5d57
rrdmp15 <rdayismar@gmail.com>
2023-06-05 09:24:04 -0500

    feat: :zap: my row
    se convirtio la fila de los productos en un web component

--- js/my-body.js
+++ js/my-body.js
@@ -2,37 +2,32 @@ import styles from "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootst
 export class myBody extends HTMLElement {
     constructor() {
         super();
+        localStorage.setItem("count", 1);
         document.adoptedStyleSheets.push(styles);
     }
     async components() {
         return await (await fetch("view/my-body.html")).text();
     }
-    selection(e) {
-        let $ = e.target;
-        if ($.nodeName == "BUTTON") {
-            let inputs = document.querySelectorAll(`#${$.dataset.row} input`);
-            if ($.innerHTML == "-") {
-                inputs.forEach(element => {
-                    if (element.name == "amount" && element.value == 0) {
-                        document.querySelector(`#${$.dataset.row}`).remove();
-                    } else if (element.name == "amount") {
-                        element.value--;
-                    }
-                });
-            } else if ($.innerHTML == "+") {
-                inputs.forEach(element => {
-                    if (element.name == "amount") {
-                        element.value++;
-                    }
-                });
-            }
-        }
+    async add(){
+        let plantilla = await (await fetch("view/my-productDetails.html")).text();
+        let id = localStorage.getItem("count");
+        
+        let render = new DOMParser().parseFromString(plantilla, "text/html")["body"];
+        render.children[0].id="product_"+id;
+        let buttons = render.querySelectorAll("button");
+        buttons.forEach(element => {
+            element.dataset.row = "product_"+id;
+        });
+   
+        document.querySelector("#products").insertAdjacentElement("beforeend", render.children[0]);
+        id++;
+        localStorage.setItem("count", id);
     }
     connectedCallback() {
         this.components().then(html => {
             this.innerHTML = html;
-            this.container = this.querySelector("#products");
-            this.container.addEventListener("click", this.selection);
+            this.add = this.querySelector("#add").addEventListener("click", this.add)
+         
         })
     }
 }
---
+++ js/my-row.js
@@ -0,0 +1,39 @@
+import styles from "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" assert { type: "css" };
+export class myRow extends HTMLElement{
+    constructor(){
+        super();
+        document.adoptedStyleSheets.push(styles);
+    }
+    async components() {
+        return await (await fetch("view/my-row.html")).text();
+    }
+    selection(e) {
+        let $ = e.target;
+        if ($.nodeName == "BUTTON") {
+            let inputs = document.querySelectorAll(`#${$.dataset.row} input`);
+            if ($.innerHTML == "-") {
+                inputs.forEach(element => {
+                    if (element.name == "amount" && element.value == 0) {
+                        document.querySelector(`#${$.dataset.row}`).remove();
+                    } else if (element.name == "amount") {
+                        element.value--;
+                    }
+                });
+            } else if ($.innerHTML == "+") {
+                inputs.forEach(element => {
+                    if (element.name == "amount") {
+                        element.value++;
+                    }
+                });
+            }
+        }
+    }
+    connectedCallback(){
+        this.components().then(html => {
+            this.innerHTML = html;
+            this.container = document.querySelector("#products");
+            this.container.addEventListener("click", this.selection);
+        })
+    }
+}
+customElements.define("my-row", myRow);
